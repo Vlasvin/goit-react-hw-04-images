c719d1cfbf6ed5caa5f9359e3171e7efee4b7515
Oleksii <103183090+Vlasvin@users.noreply.github.com>
2023-11-24 19:24:38 +0200

    modal and searchBar refactoring

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -5,19 +5,13 @@ import {
   ModalImg,
 } from 'components/Modal/Modal.styled';
 
-export const Modal = ({ closeModal, modalData }) => {
-  useEffect =
-    (() => {
-      window.addEventListener('keydown', handleKeyDown);
-    },
-    []);
-  // const componentDidMount() {
-  //   window.addEventListener('keydown', this.handleKeyDown);
-  // }
-
-  //  const componentWillUnmount() {
-  //     window.removeEventListener('keydown', this.handleKeyDown);
-  //   }
+export const Modal = ({ modalData, closeModal }) => {
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  });
 
   const handleKeyDown = e => {
     if (e.code === 'Escape') {
